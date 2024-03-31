680f732e6830232640e61ddba4eaf59320aa2c01
MochammadYusuf <mochammadyusufpratama6@gmail.com>
2024-03-31 11:43:19 +0700

    Add Feature Profile

---
+++ app/src/main/java/com/app/makanku/presentation/profile/ProfileFragment.kt
@@ -0,0 +1,61 @@
+package com.app.makanku.presentation.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.app.makanku.R
+import com.app.makanku.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+
+    private val viewModel: ProfileViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setClickListener()
+        observeEditMode()
+        observeProfileData()
+    }
+
+    private fun observeProfileData() {
+        viewModel.profileData.observe(viewLifecycleOwner) {
+            binding.ivProfile.load(it.profileImg) {
+                crossfade(true)
+                error(R.drawable.ic_profile)
+            }
+            binding.nameEditText.setText(it.name)
+            binding.usernameEditText.setText(it.username)
+            binding.emailEditText.setText(it.email)
+        }
+    }
+
+    private fun setClickListener() {
+        binding.btnEdit.setOnClickListener {
+            viewModel.changeEditMode()
+        }
+    }
+
+    private fun observeEditMode() {
+        viewModel.isEditMode.observe(viewLifecycleOwner) {
+            binding.emailEditText.isEnabled = it
+            binding.nameEditText.isEnabled = it
+            binding.usernameEditText.isEnabled = it
+        }
+    }
+
+}
